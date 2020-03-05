370401391ddc994013acdc150eb3c028d6ff9a1a
HARYSSON SOARES RODRIGUES DE QUEIROZ <h.queiroz@escolar.ifrn.edu.br>
2020-03-05 06:54:02 -0300

    create each operation function

--- Aula 02/Conceito/Prática/gabarito.js
+++ Aula 02/Conceito/Prática/gabarito.js
@@ -1,23 +1,30 @@
-let numbers = []
+let numbers = [null, null].fill(0)
 let operation
 
 const createNumber = (value) => {    
+    numbers.forEach(num => console.log(num))
     let screen = document.querySelector("#screen")
     return screen.value += value
-
 }
 
-const operation = (nums, operation = "eq", funçao) => {
-    switch(operation) {
-        case "sum":
-            return createNumber(nums[0] + nums[1])
-        case "sub":
-            return createNumber(nums[0] - nums[1])
-        case "mul":
-            return createNumber(nums[0] * nums[1])
-        case "div":
-            return createNumber(nums[0] / nums[1])
-    }
+const sum = (nums) => {
+    return createNumber(nums[0] + nums[1])
+}
+
+const sub = (nums) => {
+    return createNumber(nums[0] + nums[1])
+}
+
+const mul = (nums) => {
+    return createNumber(nums[0] + nums[1])
+}
+
+const div = (nums) => {
+    return createNumber(nums[0] + nums[1])
+}
+
+const eq = (nums) => {
+    return createNumber(nums[0] + nums[1])
 }
 
 const clean = () => {
