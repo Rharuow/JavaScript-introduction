60b40ecd6e524b049e1a879503ba86033c76426e
HARYSSON SOARES RODRIGUES DE QUEIROZ <h.queiroz@escolar.ifrn.edu.br>
2020-03-08 12:24:11 -0300

    lesson 2 Done

--- Aula 02/Conceito/Prática/gabarito.js
+++ Aula 02/Conceito/Prática/gabarito.js
@@ -1,6 +1,7 @@
 let numbers = []
 let screen 
-
+let reset = false
+let equals
 
 function operation(op) {
     if(numbers.length == 0) {
@@ -8,33 +9,38 @@ function operation(op) {
         screen.value = null
     } else {
         screen.value != null ? numbers.push(parseInt(screen.value, 10)) : numbers.push(numbers[0], 10)
+        reset = true
         switch(op) {
             case "sum":
                 screen.value = numbers[0] + numbers[1]
                 numbers = []
                 numbers[0] = parseInt(screen.value)
+                equals = "sum"
                 break;
             case "sub":
                 screen.value = numbers[0] - numbers[1]
                 numbers = []
                 numbers[0] = parseInt(screen.value)
+                equals = "sub"
                 break;
             case "mul":
                 screen.value = numbers[0] * numbers[1]
                 numbers = []
                 numbers[0] = parseInt(screen.value)
+                equals = "mul"
                 break;
             case "div":
                 screen.value = numbers[0] / numbers[1]
                 numbers = []
                 numbers[0] = parseInt(screen.value)
+                equals = "div"
                 break;
             case "clean":
                 numbers = []
                 screen.value = 0
                 break;
             case "eq":
-                operation(op)
+                operation(equals)
                 break; 
         }
     }
@@ -42,5 +48,6 @@ function operation(op) {
 
 function setValue(value) {
     screen = document.querySelector("#screen")    
-    screen.value == "0" ? screen.value = value : screen.value += value
+    screen.value == "0" || reset ? screen.value = value : screen.value += value
+    reset = false
 }
