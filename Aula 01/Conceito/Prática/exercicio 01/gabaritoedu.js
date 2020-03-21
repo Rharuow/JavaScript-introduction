3176d7912bca2d044d73850892afefa8092f7f07
eduardobcoelho <eduardobcoelho27@outlook.com>
2020-03-21 20:21:42 -0300

    exercise 01 finalized

---
+++ Aula 01/Conceito/Prática/exercicio 01/gabaritoedu.js
@@ -0,0 +1,6 @@
+function showElement(buttonValue){
+    let reader = document.querySelector("#buttonid") /* in this line it was defined that the reader takes the id's value of something */ 
+    reader.value = buttonvalue /* in this line it was defined that the reader takes the id of the value that was instantiated in this function */
+}
+
+/* so, basically in the indexedu.html it was defined - with onClick="showElement(this.value)" - that, when the button is clicked, the button's id is showed */
